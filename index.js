540406cce70c07ded3e70102590dab22cdd62145
Logan Wiser <wiserlogan@gmail.com>
2017-05-02 19:30:29 -0600

    backend changes

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const ENV = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-if (ENV !== 'production') {
+ 
   require('babel-register');
   require('babel-polyfill');
   module.exports = require('./src');
-} else {
-  module.exports = require('./dist');
-}
+
